bfcb73a3bf6a145e391bfb9d91d3df5166a8c07e
abp334 <timepass.5354@gmail.com>
2025-10-08 18:57:10 +0530

    added build.sh

---
+++ marketplace_service/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Upgrade pip and install build tools
+pip install --upgrade pip
+pip install setuptools wheel
+
+# Install the dependencies
+pip install -r requirements.txt
